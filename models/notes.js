92dd7125a24c8844e3c358acbbb93da21f19d4d3
dghero <devingreyhero@gmail.com>
2018-07-10 13:30:54 -0500

    Add mongoose, create notes schema

---
+++ models/notes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: String
+});
+
+// Add `createdAt` and `updatedAt` fields
+noteSchema.set('timestamps', true);
