19bc79e88c321b727d4a93b10f4aa651fadbc572
dghero <devingreyhero@gmail.com>
2018-07-12 13:44:27 -0500

    Add folders to database seeding

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -4,14 +4,22 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/folder');
 
 const seedNotes = require('../db/seed/notes');
+const seedFolders = require('../db/seed/folders');
 
 mongoose.connect(MONGODB_URI)
   .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(seedNotes))
+  .then(() => {
+    return Promise.all([
+      Folder.insertMany(seedFolders),
+      Folder.createIndexes,
+      Note.insertMany(seedNotes)
+    ]);
+  })
   .then(results => {
-    console.info(`Inserted ${results.length} Notes`);
+    console.info('Initialized db with notes, folders');
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
