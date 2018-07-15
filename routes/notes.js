fb0c7716e268e6f0d06d7de6ba1f22a5b390e033
dghero <devingreyhero@gmail.com>
2018-07-15 17:30:06 -0500

    Modify notes router for tags

--- routes/notes.js
+++ routes/notes.js
@@ -16,18 +16,23 @@ router.get('/', (req, res, next) => {
 
   const searchTerm = req.query.searchTerm;
   const folderId = req.query.folderId;
+  const tagId = req.query.tagId;
   let filter = {};
 
   if(searchTerm){
     filter.$or = [{title: {$regex: searchTerm, $options: 'i'}}, 
                   {content: {$regex: searchTerm, $options: 'i'}}];
   }
-  if(folderId){
-    filter.$and =[{folderId}];
-  }
+
+  //Only want the $and array if there's something to put in it
+  if(folderId || tagId) filter.$and = [];
+  //Push id filters
+  if(folderId) filter.$and.push({folderId});
+  if(tagId) filter.$and.push({ tags: {$all: [tagId]} });
   
   Note.find(filter).sort({updatedAt: 'desc'})
     .populate('folderId')
+    .populate('tags')
     .then(results =>{
       res.json(results);
     })
@@ -49,6 +54,7 @@ router.get('/:id', (req, res, next) => {
 
   Note.findById(id)
     .populate('folderId')
+    .populate('tags')
     .then(results =>{
       if(results) res.json(results);
       else next();
@@ -65,7 +71,7 @@ router.post('/', (req, res, next) => {
 
   /***** Never trust users - validate input *****/
   const newObj = {};
-  const updateableFields = ['title', 'content', 'folderId'];
+  const updateableFields = ['title', 'content', 'folderId', 'tags'];
 
   updateableFields.forEach(field => {
     if (field in req.body) {
@@ -74,13 +80,31 @@ router.post('/', (req, res, next) => {
   });
 
   /***** Never trust users - validate input *****/
+  if(newObj.tags){
+    if(!Array.isArray(newObj.tags)){
+      const err = new Error('The `tags` is not valid');
+        err.status = 400;
+        return next(err);
+    }
+
+    newObj.tags.forEach(tag =>{
+      if (!ObjectId.isValid(tag)) {
+        const err = new Error('A `tagId` is not valid');
+        err.status = 400;
+        return next(err);
+      }
+    });
+  }
+  
   if(newObj.folderId && !ObjectId.isValid(newObj.folderId)){
     const err = new Error('The `folderId` is not valid');
     err.status = 400;
     return next(err);
   }
 
-  //TODO?: Verify folderId exists in db
+  /*TODO?: Verify folderId exists in db
+           Verify tags exist in db
+   */
 
   if (!newObj.title) {
     const err = new Error('Missing `title` in request body');
@@ -109,7 +133,7 @@ router.put('/:id', (req, res, next) => {
 
   /***** Never trust users - validate input *****/
   const updateObj = {$set: {}};
-  const updateableFields = ['title', 'content', 'folderId'];
+  const updateableFields = ['title', 'content', 'folderId', 'tags'];
   let hasVal = false;
   updateableFields.forEach(field => {
     if (field in req.body) {
@@ -124,8 +148,25 @@ router.put('/:id', (req, res, next) => {
     return next(err);
   }
 
-  //TODO?: Verify folderId exists in db
+  if(updateObj.$set.tags){
+    if(!Array.isArray(updateObj.$set.tags)){
+      const err = new Error('The `tags` is not valid');
+        err.status = 400;
+        return next(err);
+    }
 
+    updateObj.$set.tags.forEach(tag =>{
+      if (!ObjectId.isValid(tag)) {
+        const err = new Error('A `tagId` is not valid');
+        err.status = 400;
+        return next(err);
+      }
+    });
+  }
+
+  /*TODO?: Verify folderId exists in db
+           Verify tags exist in db
+   */
   if(!hasVal){
     const err = new Error('No valid update fields in request body');
     err.status = 400;
