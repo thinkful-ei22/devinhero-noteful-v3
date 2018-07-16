22bcf8c14e6352bec2fc3f54a9a8458222214e89
dghero <devingreyhero@gmail.com>
2018-07-16 16:25:34 -0500

    Add secure password hashing

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,7 @@ const router = express.Router();
 router.post('/', (req,res,next) =>{
   const newObj = {};
   const inputFields = ['fullname', 'username', 'password'];
+  const authFields = ['username', 'password'];
 
   inputFields.forEach(field =>{
     if(field in req.body)
@@ -21,19 +22,49 @@ router.post('/', (req,res,next) =>{
 
   /* Trust not the user, for they will lead you astray */
 
-  if(!newObj.username){
-    const err = new Error('Missing `username` in request body');
+  inputFields.forEach(field =>{
+    if(typeof newObj[field] !== 'string' && newObj[field]){
+      const err = new Error(`Field '${field}' is not a string`);
+      err.status = 400;
+      return next(err);
+    }
+  });
+
+  authFields.forEach(field =>{
+    if(!newObj[field]){
+      const err = new Error(`Missing '${field}' in request body`);
+      err.status = 400;
+      return next(err);
+    }
+
+    if(newObj[field].charAt(0) === ' ' || newObj[field].charAt(newObj[field].length-1) === ' '){
+      const err = new Error(`Leading or trailing whitespace in '${field}'`);
+      err.status = 400;
+      return next(err);
+    }
+  });
+
+  if(newObj.username < 1){
+    const err = new Error('`username` must contain at least 1 character)');
       err.status = 400;
       return next(err);
   }
 
-  if(!newObj.password){
-    const err = new Error('Missing `password` in request body');
-    err.status = 400;
-    return next(err);
+  if(newObj.password.length < 8 || newObj.password.length > 72){
+    const err = new Error('`password` must be between 8 and 72 characters');
+      err.status = 400;
+      return next(err);    
   }
 
-  User.create(newObj)
+  return User.hashPassword(newObj.password)
+    .then(digest => {
+      const newUser = {
+        username: newObj.username,
+        password: digest,
+        fullname: newObj.fullname
+      };
+      return User.create(newUser);
+    })
     .then(results =>{
       res.location(`${req.originalUrl}/${results.id}`).status(201).json(results);
     })
